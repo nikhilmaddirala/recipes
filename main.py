9deed360b87a2da23cfdf316a3150dc2d1a663f1
Nikhil Maddirala <nikhil.maddirala@gmail.com>
2024-02-19 20:32:17 +0000

    Update front-end

--- main.py
+++ main.py
@@ -113,20 +113,21 @@ def main():
     analyzes the transcript to extract the recipe, and displays the ingredients, instructions, and
     nutritional information of the recipe.
     """
-    st.title('Recipe Extractor from YouTube Video')
+    st.title("Nikhil\'s YouTube recipe extractor")
     with st.form(key='my_form'):
         url = st.text_input('Enter YouTube URL')
         submit_button = st.form_submit_button(label='Submit')
 
     if url and submit_button:
-        # Display a message
+        # Get transcript
         st.text('Extracting transcript from YouTube url...')
         video_id = get_video_id(url)
         transcript_text = get_transcript_text(video_id)
 
-        # Display a message
+        # Extract recipe
         st.text('Analyzing transcript to extract recipe...')
         recipe_from_llm = get_recipe_from_llm(transcript_text)
+        st.text('Done! Here is the recipe:')
         
         # Convert recipe to datafrmaes
         recipe_dict = json.loads(recipe_from_llm)
@@ -135,16 +136,16 @@ def main():
         nutritional_info_df = pd.DataFrame([recipe_dict['nutritional_info']])
         nutritional_info_df = nutritional_info_df.transpose()
         
-        st.write('Ingredients:')
+        st.write('## Ingredients')
         st.dataframe(ingredients_df)
-        st.write('Instructions:')
+        st.write('## Instructions')
         st.dataframe(instructions_df)
-        st.write('Nutritional Info:')
+        st.write('## Nutritional Info')
         st.dataframe(nutritional_info_df)
 
-
+        st.write('## Full transcript')
+        st.write(f'{transcript_text}')
         st.write(f'Video ID: {video_id}')
-        st.write(f'Transcript: {transcript_text}')
 
 if __name__ == '__main__':
     main()
